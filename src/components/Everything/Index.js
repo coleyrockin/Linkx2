057927c67e38c3ea851d41083feda7c186309c80
coleyrockin <coleyrockin@aol.com>
2022-12-12 18:54:22 -0600

    edit

--- src/components/Everything/Index.js
+++ src/components/Everything/Index.js
@@ -20,7 +20,7 @@ function Everything() {
     return () => clearTimeout(intervalId);
   }, []);
   return (
-    <div class="flex flex-col mx-auto md:container md:mx-auto sm:mx-8 rounded-xl p-1 m-4 bg-gradient-to-tl from-sky-300 via-sky-600 to-emerald-500 w-11/12">
+    <div class="flex flex-col mx-auto md:container md:mx-auto sm:mx-8 rounded-xl p-1 m-4 bg-gradient-to-tl from-slate-900 via-slate-800 to-slate-900 w-11/12">
       <img src={Boyd} alt="Headshot" class="w-32 h-auto rounded-lg mt-auto mb-auto mx-auto m-1" />
       <div class="pt-2 text-center space-y-4">
         <h1 class="text-4xl  text-stone-200 m-1 ">｛Boyd Roberts｝</h1>
@@ -32,15 +32,15 @@ function Everything() {
         <h2 class="text-xl text-stone-100">Full Stack | | Developer</h2>
         <div className='md:flex flex flex-col whitespace-pre'>
           <button>
-            <a className='text-stone-400 text-xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-r from-gray-500 to-slate-500 hover:text-slate-300 focus:outline-none px-5 py-2.5 rounded-md'
+            <a className='text-stone-900 text-3xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-r from-blue-500 to-sky-300 hover:text-cyan-200 w-4/12  focus:outline-none px-5 py-2.5 rounded-md'
               href="https://github.com/coleyrockin">Github <FaGithub /></a>
           </button>
           <button>
-            <a className='text-stone-400 text-2xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-r from-slate-600 to-gray-600 hover:text-slate-300 focus:outline-none px-5 py-2.5 rounded-md'
+            <a className='text-stone-900 text-3xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-r from-blue-500 to-sky-300 hover:text-cyan-200 w-4/12  focus:outline-none px-5 py-2.5 rounded-md'
               href="https://coleyrockin.github.io/react-portfolio/">Portfolio <FaGlobe /></a>
           </button>
           <button>
-            <a className='text-stone-400 text-3xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-r from-gray-700 to-slate-700 hover:text-slate-300  focus:outline-none px-5 py-2.5 rounded-md'
+            <a className='text-stone-900 text-3xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-r from-blue-500 to-sky-300 hover:text-cyan-200 w-4/12  focus:outline-none px-5 py-2.5 rounded-md'
               href="https://www.linkedin.com/in/boydcroberts/">Linkedin <FaLinkedin /></a>
           </button>
         </div>
